33de73bebeb09c7ac8676484d6a5d25886a72432
Maria Jenny Rosali <mjrosali@Marias-MacBook-Air.local>
2025-10-03 20:13:17 +0800

    Day 1: Hello World and Variables

---
+++ day01_basics/HelloWorld.java
@@ -0,0 +1,18 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // print message
+        System.out.println("Hello, World!");
+
+        // variables
+        int age = 25;
+        double price = 19.99;
+        String name = "Jenny";
+        boolean isLearning = true;
+
+        // print variables
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Price: " + price);
+        System.out.println("Am I learning Java? " + isLearning);
+    }
+}
