07dbbb1360ae5d02bf8c41afba821da22e9c6b0f
Maria Jenny Rosali <mjrosali@Marias-MacBook-Air.local>
2025-10-04 09:33:31 +0800

    Day 2: User Input (name and age)

---
+++ day02_input/UserInput.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        scanner.nextLine(); // consumer
+
+        System.out.println(String.format("Hello, %s! You are %d years old.", name, age));
+    }
+}
