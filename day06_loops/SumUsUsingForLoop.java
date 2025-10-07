df55f979cec14f0c513fea14e61124e3c9580dc7
Maria Jenny Rosali <mjrosali@Marias-MacBook-Air.local>
2025-10-07 19:37:56 +0800

    Add Day 6 activity - sum of numbers using for loop

---
+++ day06_loops/SumUsUsingForLoop.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SumUsUsingForLoop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int numStart = scanner.nextInt();
+        int num = numStart;
+        int iStart = 1;
+
+        for (int i = iStart; i < 5; i++) {
+            num += i;
+        }
+
+        System.out.println(String.format("Sum of numbers from %d to %d is %d\n" + //
+                "", iStart, numStart, num));
+
+        scanner.close();
+    }
+}
